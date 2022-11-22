b5feb2ad3795aedc543cfbbdc0f97281c1c1649d
freeb <freeb@DESKTOP-UET03A2>
2022-11-23 02:39:28 +0900

    test

---
+++ othub2/src/main/java/com/example/demo/Othub2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Othub2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Othub2Application.class, args);
+	}
+
+}
